06a86593ac189cff5f2dd2551fa15d67068048bd
Anton Legoo <azeven@gmail.com>
2014-09-26 18:24:31 -0700

    Initial Commit

---
+++ ColorBlindViewer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ColorBlindViewer
+//
+//  Created by Anton Legoo on 9/26/14.
+//  Copyright (c) 2014 Anton Legoo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ ColorBlindViewer/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  ColorBlindViewer
+//
+//  Created by Anton Legoo on 9/26/14.
+//  Copyright (c) 2014 Anton Legoo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
